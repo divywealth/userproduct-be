eb69686be9008dbd955a6b885eca97d8744af352
Divine <divywealth1@gmail.com>
2023-06-27 23:15:48 +0100

    App Cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+    ],
+    methods: ['POST', 'PUT', 'DELETE', 'GET'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
   app.enableVersioning({
     type: VersioningType.URI,
   });
